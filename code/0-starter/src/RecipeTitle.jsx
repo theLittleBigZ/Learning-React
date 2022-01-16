c838a377f0794db694c570fd077c9c17d74cc48d
Zee Badr <zeeshan.badr@mail.utoronto.ca>
2022-01-16 13:40:25 -0500

    Module 0: Unit1-10

---
+++ code/0-starter/src/RecipeTitle.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function RecipeTitle() {
+    const title = "Mashed Potatoes"
+    return (
+        <h1>
+            {title}
+        </h1>
+    )
+};
+export default RecipeTitle;
